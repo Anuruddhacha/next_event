5031eceabd1f2e7dab149d47ceaf7550178637ca
anuruddhacha <e17038@eng.pdn.ac.lk>
2023-12-31 11:21:57 +0530

    finished version

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -1,6 +1,7 @@
 import { IEvent } from '@/lib/database/models/event.model'
 import React from 'react'
 import Card from './Card'
+import Pagination from './Pagination'
 
 type CollectionProps = {
     data: IEvent[],
@@ -51,6 +52,10 @@ type CollectionProps = {
 
 
           </ul>
+
+          {totalPages > 1 && (
+            <Pagination urlParamName={urlParamName} page={page} totalPages={totalPages} />
+          )}
 
         </div>
 
--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -35,9 +35,7 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
     const organizer = await User.findById(userId)
     if (!organizer) throw new Error('Organizer not found')
 
-    const newEvent = await Event.create(
-      { ...event, category: event.categoryId,
-         organizer: userId })
+    const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId })
     revalidatePath(path)
 
     return JSON.parse(JSON.stringify(newEvent))
@@ -101,7 +99,11 @@ export async function getAllEvents({ query, limit = 6, page, category }: GetAllE
   try {
     await connectToDatabase()
 
-    const conditions = {}
+    const titleCondition = query ? { title: { $regex: query, $options: 'i' } } : {}
+    const categoryCondition = category ? await getCategoryByName(category) : null
+    const conditions = {
+      $and: [titleCondition, categoryCondition ? { category: categoryCondition._id } : {}],
+    }
 
     const skipAmount = (Number(page) - 1) * limit
     const eventsQuery = Event.find(conditions)
